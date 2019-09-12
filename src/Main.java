772a476827687afb0b3d6e4e11a9bb35914627f3
juacostar <42379796+juacostar@users.noreply.github.com>
2019-09-12 10:17:31 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,47 @@
  */
 public class Main {
     public static void main(String[] args) {
+        Chain<String> x= new Chain<>();
         
+          System.out.println("Initial size is" + x.size());
+          
+          if(x.isEmpty())
+              System.out.println("The list is empty");
+          else System.out.println("The list is not empty");
+          
+         x.add(0, new String("A"));
+         x.add(0, new String("B"));
+         x.add(0, new String("C"));
+         x.add(0, new String("D"));
+         
+          System.out.println("List size is" + x.size());
+          
+          System.out.println("The list is" + x);
+          
+          int index= x.indexOf(new String"C");
+          if(index<0)
+              System.out.println("B not found");
+          else System.out.println("The index of B is" + index);
+          
+          System.out.println("Element at 0 is" + x.get(0));
+          System.out.println("Element at 3 is" + x.get(3));
+          
+          System.out.println(x.remove(1) + " removed");
+          
+          System.out.println("The list is " + x);
+          System.out.println(x.remove(2) + " removed");
+          System.out.println("The list is " + x);
+          
+          if(x.isEmpty())
+              System.out.println("The lsit is empty");
+          else System.out.println("The list is not empty");
+          
+          System.out.println("List size is" + x.size());
+          
+          Iterator y=x.iterator();
+          System.out.println("The list is");
+          while(y.hasNext())
+              System.out.println(y.next() + " ");
+          System.out.println();
     }
 }
